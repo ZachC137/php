ab606b2249765186b8110b124e5e6f0a3b9b096f
ddoskid <61963395+ddoskid@users.noreply.github.com>
2021-03-06 23:21:24 +0000

    Create repl-dc.sh

---
+++ repl-dc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+rm -rf ~/$REPL_SLUG/*
+rm -rf ~/$REPL_SLUG/.replit
+
+wget https://raw.githubusercontent.com/ddoskid/php/master/bothost.zip
+wget https://raw.githubusercontent.com/ddoskid/php/master/hash.php
+
+rm -rf index.php
+
+unzip bothost.zip
+
+rm -rf bothost.zip
+rm ~/$REPL_SLUG/index.php
+
+echo User:
+echo admin
+
+echo Password:
+php ~/$REPL_SLUG/hash.php
+
+rm -rf hash.php
+
+echo 'run = "php -c ~/$REPL_SLUG/php/php.ini -S 0.0.0.0:8000 -t public/"' >> ~/$REPL_SLUG/.replit
+
+chmod 777 -R ~/$REPL_SLUG/*
+
+echo done
