2ed1d1a1c9737dcd73b378b777844c89aeb01042
ddoskid <61963395+ddoskid@users.noreply.github.com>
2021-03-06 23:16:10 +0000

    Create hash.php

---
+++ hash.php
@@ -0,0 +1,26 @@
+<?php
+
+function randomPassword() {
+    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 16; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+
+$password = randomPassword();
+
+$hashed = password_hash($password, PASSWORD_DEFAULT);
+
+echo $password;
+
+function replace_string_in_file($filename, $string_to_replace, $replace_with){
+    $content=file_get_contents($filename);
+    $content_chunks=explode($string_to_replace, $content);
+    $content=implode($replace_with, $content_chunks);
+    file_put_contents($filename, $content);
+}
+replace_string_in_file('public/files.php', 'passwordset', $hashed);
