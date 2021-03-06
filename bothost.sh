85ad94d1121ba549a136fb61f035fb628f3aae3f
ddoskid <61963395+ddoskid@users.noreply.github.com>
2021-03-06 16:49:37 +0000

    Update bothost.sh

--- bothost.sh
+++ bothost.sh
@@ -8,6 +8,6 @@ unzip bothost.zip
 
 rm -rf bothost.zip
 
-echo "run = "php -c ~/$REPL_SLUG/php/php.ini -S 0.0.0.0:8000 -t public/"" > .replit
+echo 'run = "php -c ~/$REPL_SLUG/php/php.ini -S 0.0.0.0:8000 -t wordpress/"' >> ~/$REPL_SLUG/.replit
 
 echo done
